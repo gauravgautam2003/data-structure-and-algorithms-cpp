5062e359a8fae6dbdef9851aefc938c81887b609
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-08-01 15:07:11 +0530

    add recursive power function implementation

---
+++ recursion/power.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int power(int a, int b) {
+    // base case
+    if(b == 0) return 1;
+    if(b == 1) return a; 
+
+    int ans = power(a, b/2);
+
+    if(b % 2 == 0) {
+        ans = ans * ans;
+    }else {
+        ans = a * ans * ans;
+    }
+
+    return ans;
+}
+
+
+int main() {
+    int a = 3, b = 2;
+    int result = power(a, b);
+    cout << result;
+    return 0;
+}
