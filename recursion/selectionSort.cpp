11f4cd3852be81bb7d274ce1a3e0895e4c01884a
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-08-01 17:27:52 +0530

    implement selection sort algorithm using recursion

---
+++ recursion/selectionSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int selectionSort(int *arr, int size) {
+    // base case
+
+    if(size == 0 || size == 1) return 0;
+
+    //processing
+    int maxIndex = 0;
+    for(int i = 1; i < size; i++) {
+        if(arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    // swap
+    int temp = arr[maxIndex];
+    arr[maxIndex] = arr[size - 1]; 
+    arr[size - 1] = temp; 
+
+
+    // recursive call
+    selectionSort(arr, size - 1);
+
+}
+
+int main() {
+    int arr[5] = {45,23,2,54,8};
+    int size = sizeof(arr)/sizeof(int);
+
+    selectionSort(arr, size);
+
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
