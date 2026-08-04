3f2981a8382968b6af57a9bcc19055644f2ffcda
gaurav gautam <gauravgautam9865@gmail.com>
2026-08-04 23:55:12 +0530

    Time: 2 ms (81.52%), Space: 11.3 MB (84.22%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -5,13 +5,14 @@ public:
         char arr[10000];
         size_t len = s.length();
         int index = 0;
+        int steps = 2 * (numRows - 1);
 
         for(int i = 0; i < numRows; i++) {
-            for(int j = i; j < len; j = j + 2*(numRows - 1)) {
+            for(int j = i; j < len; j = j + steps) {
                 arr[index++] = s[j];
 
-                if(i > 0 && i < numRows - 1 && j + 2*(numRows - 1) - 2*i < len) {
-                    arr[index++] = s[j + 2*(numRows - 1) - 2*i];
+                if(i > 0 && i < numRows - 1 && j + steps - 2*i < len) {
+                    arr[index++] = s[j + steps - 2*i];
                 }
             }
         }
